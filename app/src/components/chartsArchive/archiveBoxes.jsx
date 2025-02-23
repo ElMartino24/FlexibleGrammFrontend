d058934c28cb15768540090afe5987cd3b20b64d
ElMartino24 <86724612+ElMartino24@users.noreply.github.com>
2025-02-23 08:31:04 +0100

    Update archiveBoxes.jsx

--- app/src/components/chartsArchive/archiveBoxes.jsx
+++ app/src/components/chartsArchive/archiveBoxes.jsx
@@ -12,9 +12,6 @@ function ArchiveBoxes() {
           <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
             Flow Charts
           </h3>
-          <p className="font-arial text-black text-xs sm:sm text-left">
-            Search for Flow Charts
-          </p>
         </div>
       </Link>
       <Link
@@ -25,9 +22,6 @@ function ArchiveBoxes() {
           <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
             Bar Charts
           </h3>
-          <p className="font-arial text-black text-xs sm:sm  text-left">
-            Search for Bar Charts
-          </p>
         </div>
       </Link>
       <Link
@@ -38,9 +32,6 @@ function ArchiveBoxes() {
           <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
             Column Charts
           </h3>
-          <p className="font-arial text-black text-xs sm:sm  text-left">
-            Search for Column Charts
-          </p>
         </div>
       </Link>
       <Link
@@ -51,9 +42,16 @@ function ArchiveBoxes() {
           <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
             Coming Soon...
           </h3>
-          <p className="font-arial text-black text-xs sm:sm  text-left">
+        </div>
+      </Link>
+      <Link
+        to="/"
+        className="flex items-center bg-[#F5F5F5] border-2 border-solid border-black hover:bg-[white] rounded-xl p-1 max-lg:p-3 mb-2 cursor-pointer no-underline pl-[5%] mb-[2rem]"
+      >
+        <div className="flex flex-col">
+          <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
             Coming Soon...
-          </p>
+          </h3>
         </div>
       </Link>
       <Link
@@ -62,25 +60,8 @@ function ArchiveBoxes() {
       >
         <div className="flex flex-col">
           <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
-            News / Announcements
+            Coming Soon...
           </h3>
-          <p className="font-arial text-black text-xs sm:sm text-left">
-            Here you can find all the news.
-          </p>
-        </div>
-      </Link>
-      <Link
-        to="/"
-        className="flex items-center bg-[#F5F5F5] border-2 border-solid border-black hover:bg-[white] rounded-xl p-1 max-lg:p-3 mb-2 cursor-pointer no-underline pl-[5%] mb-[2rem]"
-      >
-        <div className="flex flex-col">
-          <h3 className="font-arial text-black text-1xl sm:text-2xl text-left">
-            Suggestions - Critique - Errors
-          </h3>
-          <p className="font-arial text-black text-xs sm:sm  text-left">
-            Please write your (constructive criticism) or improvement
-            suggestions here.
-          </p>
         </div>
       </Link>
     </div>
