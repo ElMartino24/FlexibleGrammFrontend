5d8e292d7d5dcc151d382c4f413fc05c74cebfe1
ElMartino24 <86724612+ElMartino24@users.noreply.github.com>
2025-02-28 10:23:56 +0100

    Update chartsArchive.jsx

--- app/src/components/chartsArchive/chartsArchive.jsx
+++ app/src/components/chartsArchive/chartsArchive.jsx
@@ -4,10 +4,6 @@ import ArchiveBoxes from "./archiveBoxes";
 function ChartsArchive() {
   return (
     <div className="mb-[12%]">
-      {/* <Header /> */}
-      {/* <Cummunity />
-      <DimTravler />
-      <CreativitySpace /> */}
       <ArchiveBoxes />
     </div>
   );
